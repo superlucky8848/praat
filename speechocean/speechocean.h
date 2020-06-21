ed274148a585c6aa35f0c256b432fca69f451526
superlucky <mail.superlucky@gmail.com>
2020-06-21 16:35:22 +0800

    remove unused variables.

--- speechocean/speechocean.h
+++ speechocean/speechocean.h
@@ -8,8 +8,6 @@
 
 autostring32 get_current_time_string()
 {
-    autostring32 result(25);
-
     std::chrono::high_resolution_clock::time_point nowTime = std::chrono::high_resolution_clock::now();
     int milli = static_cast<int>(
         std::chrono::duration_cast<std::chrono::milliseconds>(nowTime.time_since_epoch()).count() % 1000);
@@ -22,6 +20,28 @@ autostring32 get_current_time_string()
     return Melder_8to32(buffer);
 }
 
+void Speechocean_Write_Log(conststring32 log_file_name, bool is_log_time, conststring32 user_id, conststring32 message)
+{
+    structMelderFile _file { };
+    MelderFile file = &_file;        
+
+    Melder_relativePathToFile(log_file_name, &_file);
+    if(is_log_time)
+    {
+        autostring32 timeStr=get_current_time_string();
+        MelderFile_appendText(file, timeStr.get());
+        MelderFile_appendText(file, U"\t");
+    }
+
+    MelderFile_appendText(file, user_id);
+    MelderFile_appendText(file, U"\t");
+
+    MelderFile_appendText(file, message);
+    MelderFile_appendText(file, U"\n");
+
+    MelderFile_close(file);
+}
+
 FORM (HELP_Write_Log, U"SpeechOcean Write Log", nullptr)
 {
     SENTENCE(log_file_name, U"Log File Name", U"log.txt")
@@ -30,25 +50,7 @@ FORM (HELP_Write_Log, U"SpeechOcean Write Log", nullptr)
     SENTENCE(message, U"Log Message", U"Log Test, Speechocean")
     OK
     DO
-        structMelderFile _file { };
-        MelderFile file = &_file;        
-        
-
-        Melder_relativePathToFile(log_file_name, &_file);
-        if(is_log_time)
-        {
-            autostring32 timeStr=get_current_time_string();
-            MelderFile_appendText(file, timeStr.get());
-            MelderFile_appendText(file, U"\t");
-        }
-
-        MelderFile_appendText(file, user_id);
-        MelderFile_appendText(file, U"\t");
-
-        MelderFile_appendText(file, message);
-        MelderFile_appendText(file, U"\n");
-
-        MelderFile_close(file);
+        Speechocean_Write_Log(log_file_name, is_log_time, user_id, message);
 	    //Melder_information (message);
     END
 }
